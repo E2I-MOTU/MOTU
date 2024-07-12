bc2b612fb0c3b1314eb0c4620eec27f133ba3af9
hgkim215 <hgkim215@gmail.com>
2024-07-13 02:56:58 +0900

    [Feat] 파이어베이스 스토리지 CSV 파일 연결 성공 및 차트 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:motu/controller/scenario_service.dart';
 import 'package:motu/firebase_options.dart';
-import 'package:motu/view/app.dart';
-import 'package:motu/view/live_chart.dart';
+import 'package:motu/view/scenario/scenario_chart.dart';
+import 'package:motu/view/scenario/scenario_list.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +12,14 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ScenarioService()),
+      ],
+      builder: (context, child) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +35,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const InitialPage(),
-      home: const LiveChart(),
+      home: const ScenarioList(),
     );
   }
 }
---
+++ lib/view/scenario/scenario_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:motu/controller/scenario_service.dart';
+import 'package:motu/view/scenario/scenario_chart.dart';
+
+class ScenarioList extends StatelessWidget {
+  const ScenarioList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scenario List'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ScenarioService();
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const ScenarioChart()));
+          },
+          child: const Text("TEST"),
+        ),
+      ),
+    );
+  }
+}
