38bc7c4f731d7c49a9b4a6b0b8caa6b96e2902e3
박승리 <victopac@naver.com>
2024-07-16 20:53:47 +0900

    [Feat] 홈화면 와이어프레임 구현(페이지 연결 X)

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -74,59 +74,118 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              SizedBox(
-                height: 200,
-                width: double.infinity,
-                child: PageView.builder(
-                  onPageChanged: (value){
-                    setState(() {
-                      currentSlide = value;
-                    });
-                  },
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    return Container(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    SizedBox(
                       height: 200,
                       width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey,
+                      child: PageView.builder(
+                        onPageChanged: (value) {
+                          setState(() {
+                            currentSlide = value;
+                          });
+                        },
+                        itemCount: 4,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 200,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.grey,
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ),
-              Positioned.fill(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(
-                      4,
-                          (index) => AnimatedContainer(
-                        duration: const Duration(milliseconds: 300),
-                        width: currentSlide == index ? 15 : 8,
-                        height: 8,
-                        margin: const EdgeInsets.only(right: 3),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: currentSlide == index
-                                ? Colors.black
-                                : Colors.transparent,
-                            border: Border.all(color: Colors.black)
+                    ),
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List.generate(
+                              4,
+                                  (index) => AnimatedContainer(
+                                duration: const Duration(milliseconds: 300),
+                                width: currentSlide == index ? 15 : 8,
+                                height: 8,
+                                margin: const EdgeInsets.only(right: 3),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: currentSlide == index
+                                      ? Colors.black
+                                      : Colors.transparent,
+                                  border: Border.all(color: Colors.black),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
+                  ],
+                ),
+
+                SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "오늘의 추천 학습",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("전체보기"),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(4, (index) {
+                      return AspectRatio(
+                        aspectRatio: 2.6 / 3,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 15.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      );
+                    }),
                   ),
                 ),
-              ),
-            ],
+
+                SizedBox(height: 30,),
+
+                Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
