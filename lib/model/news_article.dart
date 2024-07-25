fe0ad5d7dff09f24a8f4bb0ffa878ed06015367a
JeongSuA0724 <22000672@handong.ac.kr>
2024-07-25 10:58:45 +0900

    [Feat] #16 뉴스 api 연결, url_launcher 안되는 문제 있음

--- lib/model/news_article.dart
+++ lib/model/news_article.dart
@@ -1,13 +1,22 @@
 class NewsArticle {
   final String title;
-  final String description;
-  final String imageUrl;
-  final String topic;
+  final String press;
+  final String summary;
+  final String link;
 
   NewsArticle({
     required this.title,
-    required this.description,
-    required this.imageUrl,
-    required this.topic,
+    required this.press,
+    required this.summary,
+    required this.link,
   });
+
+  factory NewsArticle.fromJson(Map<String, dynamic> json) {
+    return NewsArticle(
+      title: json['title'] ?? 'No Title',
+      press: json['publisher'] ?? 'No Publisher',
+      summary: json['description'] ?? 'No Description',
+      link: json['link'] ?? '',
+    );
+  }
 }
