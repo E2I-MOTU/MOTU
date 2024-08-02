35e6ea1289075ed60d5b52553e3a14ab83cfc6d1
박승리 <victopac@naver.com>
2024-08-02 13:49:03 +0900

    [Chore] UI 개선

--- lib/view/quiz/quiz_screen.dart
+++ lib/view/quiz/quiz_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:motu/view/quiz/widget/linear_indicator.dart';
+import 'package:motu/view/theme/color_theme.dart';
 import 'package:provider/provider.dart';
 import '../../provider/quiz_provider.dart';
 import 'incorrect_answers_screen.dart';
@@ -74,97 +76,111 @@ class QuizScreen extends StatelessWidget {
             ),
             body: Stack(
               children: [
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          question['question'] ?? '질문이 없습니다.',
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      ...(question['options'] as List<dynamic>).map<Widget>((option) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 16.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              onPressed: quizState.answered
-                                  ? null
-                                  : () {
-                                quizState.selectAnswer(option as String);
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: quizState.selectedAnswer == option
-                                    ? Colors.deepPurpleAccent
-                                    : null,
-                                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
-                                  ),
+                Column(
+                  children: [
+                    LinearIndicator(
+                      current: quizState.currentQuestionIndex + 1,
+                      total: quizState.questions.length,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                question['question'] ?? '질문이 없습니다.',
+                                style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              child: Text(option as String),
                             ),
-                          ),
-                        );
-                      }).toList(),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: SizedBox(
-                            width: 150,
-                            child: ElevatedButton(
-                              onPressed: quizState.selectedAnswer.isEmpty
-                                  ? null
-                                  : () {
-                                quizState.submitAnswer(question['answer'] ?? '');
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text(quizState.correct ? '정답입니다!' : '오답입니다.'),
-                                      content: SizedBox(
-                                        width: MediaQuery.of(context).size.width * 0.7,
-                                        child: Text(quizState.correct
-                                            ? '잘했어요!'
-                                            : '정답은 ${question['answer']} 입니다.'),
-                                      ),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          child: const Text('다음 질문'),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                            quizState.nextQuestion(uid, collectionName);
-                                          },
+                            ...(question['options'] as List<dynamic>).map<Widget>((option) {
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    onPressed: quizState.answered
+                                        ? null
+                                        : () {
+                                      quizState.selectAnswer(option as String);
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: quizState.selectedAnswer == option
+                                          ? ColorTheme.colorPrimary
+                                          : ColorTheme.colorWhite,
+                                      foregroundColor: quizState.selectedAnswer == option
+                                          ? ColorTheme.colorWhite
+                                          : ColorTheme.colorFont,
+                                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
                                         ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
+                                      ),
+                                    ),
+                                    child: Text(option as String),
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: SizedBox(
+                                  width: 150,
+                                  child: ElevatedButton(
+                                    onPressed: quizState.selectedAnswer.isEmpty
+                                        ? null
+                                        : () {
+                                      quizState.submitAnswer(question['answer'] ?? '');
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: Text(quizState.correct ? '정답입니다!' : '오답입니다.'),
+                                            content: SizedBox(
+                                              width: MediaQuery.of(context).size.width * 0.7,
+                                              child: Text(quizState.correct
+                                                  ? '잘했어요!'
+                                                  : '정답은 ${question['answer']} 입니다.'),
+                                            ),
+                                            actions: <Widget>[
+                                              TextButton(
+                                                child: const Text('다음 질문'),
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                  quizState.nextQuestion(uid, collectionName);
+                                                },
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Text('제출'),
                                   ),
                                 ),
                               ),
-                              child: const Text('제출'),
                             ),
-                          ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
                 Positioned(
                   bottom: 16,
