9a72144b5d41bc90c0ac3aa0c39afe0ab6c22563
박승리 <victopac@naver.com>
2024-08-09 13:34:10 +0900

    [Refactor] 텍스트 줄바꿈 함수 파일 분리

--- lib/view/terminology/widget/terminology_card_builder.dart
+++ lib/view/terminology/widget/terminology_card_builder.dart
@@ -1,26 +1,9 @@
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_balloon/speech_balloon.dart';
+import '../../../text_utils.dart';
 import '../../theme/color_theme.dart';
 
-final RegExp emoji = RegExp(
-    r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])');
-
-// 단어를 \u200D로 연결하는 함수
-String preventWordBreak(String text) {
-  List<String> words = text.split(' ');
-  String fullText = '';
-
-  for (var i = 0; i < words.length; i++) {
-    fullText += emoji.hasMatch(words[i])
-        ? words[i]
-        : words[i]
-        .replaceAllMapped(RegExp(r'(\S)(?=\S)'), (m) => '${m[1]}\u200D');
-    if (i < words.length - 1) fullText += ' ';
-  }
-
-  return fullText;
-}
 
 Widget buildTermCard(BuildContext context, String term, String definition, String example, bool isBookmarked, VoidCallback onBookmarkToggle) {
   final size = MediaQuery.of(context).size;
