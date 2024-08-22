9fa1d6d2d4c8ed581b0aeb67134d4a2c3e38ddae
박승리 <victopac@naver.com>
2024-08-22 20:14:09 +0900

    [Chore] 경제꿀팁 UI 수정 및 디바이더 추가

--- lib/view/article/widget/article_list_builder.dart
+++ lib/view/article/widget/article_list_builder.dart
@@ -12,86 +12,104 @@ Future<String> getImageUrl(String imagePath) async {
   }
 }
 
-Widget articleListBuilder(BuildContext context, Article article) {
-  return InkWell(
-    onTap: () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ArticleDetailScreen(article: article),
-        ),
-      );
-    },
-    child: Card(
-      margin: EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          // 이미지
-          Container(
-            width: 90,
-            height: 90,
-            padding: EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: FutureBuilder(
-                future: getImageUrl(article.imageUrl),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasError || !snapshot.hasData || snapshot.data == '') {
-                    return Icon(Icons.error);
-                  } else {
-                    return Image.network(
-                      snapshot.data!,
-                      fit: BoxFit.cover,
-                    );
-                  }
-                },
-              ),
+Widget articleListBuilder(BuildContext context, Article article, {bool showDivider = true}) {
+  return Column(
+    children: [
+      InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ArticleDetailScreen(article: article),
             ),
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 8.0),
-                    child: Wrap(
-                      spacing: 8.0,
-                      children: article.topics.map((topic) {
-                        return Container(
-                          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                          decoration: BoxDecoration(
-                            color: Color(0xff701FFF),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Text(
-                            topic,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 10,
-                            ),
-                          ),
+          child: Row(
+            children: [
+              // 이미지
+              Container(
+                width: 90,
+                height: 90,
+                padding: EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: FutureBuilder<String>(
+                    future: getImageUrl(article.imageUrl),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError || !snapshot.hasData || snapshot.data == '') {
+                        return Center(child: Icon(Icons.error));
+                      } else {
+                        return Image.network(
+                          snapshot.data!,
+                          fit: BoxFit.cover,
                         );
-                      }).toList(),
-                    ),
+                      }
+                    },
                   ),
-                  // 제목
-                  Text(
-                    article.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 8.0),
+                        child: Wrap(
+                          spacing: 8.0,
+                          children: article.topics.map((topic) {
+                            return Container(
+                              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                              decoration: BoxDecoration(
+                                color: Color(0xff701FFF),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Text(
+                                topic,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      // 제목
+                      Text(
+                        article.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-    ),
+      // Custom Divider
+      if (showDivider)
+        LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              width: constraints.maxWidth * 0.9, // 90% of the screen width
+              height: 1.0,
+              color: Colors.grey[300],
+            );
+          },
+        ),
+    ],
   );
 }
